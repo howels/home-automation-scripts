b7ee67bdbcf42c69e74b5c11613b2316812c000f
howels <howels@users.noreply.github.com>
2015-05-24 02:27:26 +0100

    Create ener_s1_off.sh

---
+++ ener_s1_off.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+ echo 0 > /sys/class/gpio/gpio17/value
+ echo 0 > /sys/class/gpio/gpio22/value
+ echo 0 > /sys/class/gpio/gpio23/value
+ echo 0 > /sys/class/gpio/gpio27/value
+ echo 0 > /sys/class/gpio/gpio25/value
+ echo 0 > /sys/class/gpio/gpio24/value
+ sleep 0.1
+ echo 1 > /sys/class/gpio/gpio17/value
+ echo 1 > /sys/class/gpio/gpio22/value
+ echo 1 > /sys/class/gpio/gpio23/value
+ echo 0 > /sys/class/gpio/gpio27/value
+ sleep 0.1
+ echo 1 > /sys/class/gpio/gpio25/value
+ sleep 0.5
+ echo 0 > /sys/class/gpio/gpio25/value
